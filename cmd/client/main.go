e7de9321cdab3ffb7c5567eb565179a200e0ef78
Felipe Ferreira <me@felipefbs.dev>
2022-03-14 19:39:41 -0300

    feat: added client

---
+++ cmd/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/felipefbs/grpc/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "123",
+		Name:  "felipefbs",
+		Email: "a@a.com",
+	}
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+}
